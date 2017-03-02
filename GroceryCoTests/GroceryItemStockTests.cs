b0e960523b54ae87328ad41a5a0913f830e5f2ad
Nigel John <johnn3@mymacewan.ca>
2017-03-02 02:53:46 -0700

    Add and renamed test cases

---
+++ GroceryCoTests/GroceryItemStockTests.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApplication1;
+using System;
+using NUnit.Framework;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestFixture()]
+    public class GroceryItemStockTests
+    {
+        [Test()]
+        public void GroceryItemStockTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void CheckStockTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetItemPriceTest()
+        {
+            Assert.Fail();
+        }
+    }
+
+    [TestFixture()]
+    public class UnitTest1
+    {
+        [Test()]
+        public void GroceryItemStockTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
