b705eab66c2b32a8fc83b9b0d5ead412a1539209
Nigel John <johnn3@mymacewan.ca>
2017-03-02 09:39:47 -0700

    changed classes to public explicitly

--- ConsoleApplication1/ReceiptController.cs
+++ ConsoleApplication1/ReceiptController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class ReceiptController
+    public class ReceiptController
     {
         private Receipt _model;
         private ReceiptView _view;
--- ConsoleApplication1/ReceiptView.cs
+++ ConsoleApplication1/ReceiptView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class ReceiptView
+    public class ReceiptView
     {
         public ReceiptView() { }
 
