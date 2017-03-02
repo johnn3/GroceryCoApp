1ca3f7e8388bb5e223e1bf190d6631b0ea892eee
Nigel John <johnn3@mymacewan.ca>
2017-03-02 02:17:57 -0700

    transfered changes from master branch into check branch

--- ConsoleApplication1/GroceryItemStock.cs
+++ ConsoleApplication1/GroceryItemStock.cs
@@ -1,6 +1,32 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
 {
     internal class GroceryItemStock
     {
+        private Dictionary<string, decimal> _itemStock;
+
+        public GroceryItemStock(Dictionary<string, decimal> itemStock)
+        {
+            _itemStock = itemStock;
+        }
+
+        public bool CheckStock(string item)
+        {
+            return _itemStock.ContainsKey(item);
+        }
+
+        public decimal GetItemPrice(string item)
+        {
+            decimal value;
+
+            if (_itemStock.TryGetValue(item, out value))
+            {
+                return value;
+            }
+
+            return 0.00m;
+        }
     }
 }
